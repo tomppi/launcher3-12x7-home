52e2df022f0b894f74afa395850f1c5564400eb2
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-14 16:52:07 +0300

    Add app drawer widget Android module

---
+++ drawer-widget/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "io.github.tomppi.drawerwidget"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "io.github.tomppi.drawerwidget"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
