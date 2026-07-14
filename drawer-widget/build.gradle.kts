2402b873d8e88714b9d72dc2edcb6e7c7f410182
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-14 17:12:36 +0300

    Bump drawer widget to 1.0.2

--- drawer-widget/build.gradle.kts
+++ drawer-widget/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.tomppi.drawerwidget"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
