8695ba6e9504a7f18b7173fbbe4c88d96cd04d85
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-14 08:44:59 +0300

    Bump module version for widget and foldable changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.tomppi.launchergrid712"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 
     buildTypes {
