7baa6b18f0d793bf1b4b1bb9446f9626dd927be7
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-14 08:44:47 +0300

    Extend Xposed stubs for widget and feature-flag hooks

--- tools/xposed-api-stubs/src/de/robv/android/xposed/XposedHelpers.java
+++ tools/xposed-api-stubs/src/de/robv/android/xposed/XposedHelpers.java
@@ -2,7 +2,11 @@ package de.robv.android.xposed;
 
 public final class XposedHelpers {
     private XposedHelpers() {}
+
     public static Class<?> findClass(String className, ClassLoader classLoader) { return null; }
     public static Object getObjectField(Object obj, String fieldName) { return null; }
+    public static Object getStaticObjectField(Class<?> clazz, String fieldName) { return null; }
     public static void setIntField(Object obj, String fieldName, int value) {}
+    public static void setBooleanField(Object obj, String fieldName, boolean value) {}
+    public static void setStaticObjectField(Class<?> clazz, String fieldName, Object value) {}
 }
